e838a40583b5752d7fef33b7453a98ed35e9dfdb
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-08-06 14:34:31 -0700

    added sanity checks per Fred

--- tests/test.py
+++ tests/test.py
@@ -22,6 +22,10 @@ def test_response():
     assert response['image_size'] == [513, 256]
     assert len(response['seg_map']) == response['image_size'][1]
 
+    assert response['seg_map'][0][0] == 0  # there are no objects in the top left corner
+    assert response['seg_map'][128][128] == 15  # there is a person here
+    assert response['seg_map'][200][500] == 20  # computer monitor (labeled as "TV") in bottom right corner
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
