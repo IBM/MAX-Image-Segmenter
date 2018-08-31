77ad1b12cd4c90dfd71f025c44093f44100b66f4
Nick Kasten <kastencode@gmail.com>
2018-08-30 21:10:18 -0500

    moved CORS terminal output to app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import os
+import sys
 from flask import Flask
 from api import api
 from flask_cors import CORS
@@ -12,8 +14,13 @@ if 'APP_CONFIG' in os.environ:
     app.config.from_envvar('APP_CONFIG')
 api.init_app(app)
 # enable CORS if flag set in config
-if os.getenv('CORS_ENABLE') == 'true':
+if os.getenv('CORS_ENABLE') == 'true' and \
+os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
     CORS(app, origins='*')
+    print('\033[92m' + \
+    'NOTE: MAX model server is currently allowing cross-origin requests - ' + \
+    '\033[1m' + '(CORS ENABLED)' + '\033[0m' + \
+    '\033[0m', file=sys.stderr)    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
